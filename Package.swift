78d02351fe3c3530d5335b65f8ac79662c3265b5
The iOS Team @ GeneXus <builder@genexus.com>
2025-03-11 14:21:32 -0300

    Package version: 3.1.0-beta.8

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_BeaconsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "3.1.0-beta.7")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "3.1.0-beta.8")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_BeaconsWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Beacons",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Beacons-3.1.0-beta.7.xcframework.zip",
-			checksum: "872c630f4de01f77250c14c0e2f30f96a495f05529f56c0e5cf993dc47028c02"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Beacons-3.1.0-beta.8.xcframework.zip",
+			checksum: "c51270dc412dfdb05337758dc16ab24885cb44685965fc1477de1ff1df792ae4"
 		)
 	]
 )
