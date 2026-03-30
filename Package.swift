8d45664a5e8c08c2ec0828ced8d6d588a19210eb
The iOS Team @ GeneXus <builder@genexus.com>
2026-03-30 13:34:55 -0400

    Package version: 4.2.0-rc.0

--- Package.swift
+++ Package.swift
@@ -1,36 +1,28 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXCoreModule_SD_Beacons",
-	platforms: [.iOS("12.0")],
+	platforms: [.iOS("15.0")],
 	products: [
 		.library(
 			name: "GXCoreModule_SD_Beacons",
 			targets: ["GXCoreModule_SD_BeaconsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.2.0-rc.0")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_BeaconsWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Beacons",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS])),
-					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.iOS])),
-					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
-					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS])),
-					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.iOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Beacons",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Beacons-1.1.0.xcframework.zip",
-			checksum: "4aee6d1495869dff4646e8653ae61c71649e19062ffa01486c2078f2c1d54f6e"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreModule_SD_Beacons-4.2.0-rc.0.xcframework.zip",
+			checksum: "ce70f65327e80e311a76980a2a6ff10543f2c61c3e3b10a77ddf6472812bc070"
 		)
 	]
 )
